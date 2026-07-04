1358db16718a6b16793a304f6171f876487dcd80
tasnimferdous <tasnimferdous216@gmail.com>
2026-07-04 09:25:05 +0600

    update in JwtAuthenticationFilter

---
+++ src/main/java/com/project/security/model/UserPrincipal.java
@@ -0,0 +1,13 @@
+package com.project.security.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPrincipal {
+    private String userId;
+    private String username;
+}
